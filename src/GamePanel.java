62ad1289320e4d4f9e70fb980ac50b94be0ad4da
Rita Guimarães <kikasgui@gmail.com>
2022-01-31 21:49:35 +0000

    Update GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -23,7 +23,6 @@ public class GamePanel extends JPanel implements ActionListener {
     private char direction = 'R';
     private boolean running = false;
     Timer timer;
-    //Random random;
 
     GamePanel() {
         var random = new Random();
@@ -60,6 +59,17 @@ public class GamePanel extends JPanel implements ActionListener {
         g.setColor(Color.red);
         g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
 
+        for(int i = 0; i < bodyParts; i++) {
+            if(i == 0) {
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            else {
+                g.setColor(new Color(45,180,0));
+                g.fillRect(x[i],y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
+
     }
 
     public void newApple() {
@@ -87,6 +97,12 @@ public class GamePanel extends JPanel implements ActionListener {
 
             case 'L':
                 x[0] = x[0] - UNIT_SIZE;
+                break;
+
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+                
         }
     }
 
@@ -96,6 +112,36 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void checkCollisions() {
 
+        // checks if the head collides with the body
+        for(int i = bodyParts; i > 0; i--) {
+            if((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+            }
+        }
+
+        // checks if head touches left border
+        if(x[0] < 0) {
+            running = false;
+        }
+
+        // checks if head touches right border
+        if(x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+
+        // check if head touches top border
+        if(y[0] < 0) {
+            running = false;
+        }
+
+        // check if head touches bottom border
+        if(y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
+
+        if(!running) {
+            timer.stop();
+        }
     }
 
     public void gameOver(Graphics g) {
@@ -106,12 +152,44 @@ public class GamePanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        if(running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
+
     }
 
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
 
+            switch(e.getKeyCode()) {
+
+                case KeyEvent.VK_LEFT:
+                    if(direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+
+                case KeyEvent.VK_RIGHT:
+                    if(direction != 'L') {
+                        direction = 'R';
+                    }
+
+                case KeyEvent.VK_UP:
+                    if(direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+
+                case KeyEvent.VK_DOWN:
+                    if(direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+            }
         }
     }
 }
